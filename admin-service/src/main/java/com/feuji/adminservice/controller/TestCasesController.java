a085eed0b9ce1ed35f2b5681668d88712b18bf4b
Suresh-130 <suresh.kuruva14335@gmail.com>
2023-05-03 18:05:12 +0530

    status

--- admin-service/src/main/java/com/feuji/adminservice/controller/TestCasesController.java
+++ admin-service/src/main/java/com/feuji/adminservice/controller/TestCasesController.java
@@ -6,13 +6,16 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.feuji.adminservice.service.TestCasesServices;
+import com.feuji.commonmodel.Question;
 import com.feuji.commonmodel.TestCases;
 
 @RestController
@@ -23,8 +26,8 @@ public class TestCasesController
 	private TestCasesServices testCasesServices;
 	
 	@PostMapping("/addtestcases/{qid}")
-	public HttpStatus insertQuestion(@RequestBody List<TestCases> testCasesList, @PathVariable Long qid) {
-		testCasesServices.addTestCases(testCasesList,qid);
+	public HttpStatus insertQuestion(@RequestBody TestCases testCases, @PathVariable Long qid) {
+		testCasesServices.addTestCases(testCases,qid);
 		return HttpStatus.OK;
 	}
 	
@@ -33,6 +36,19 @@ public class TestCasesController
 	{
 		
 		return testCasesServices.getAllTestCases(qid);
+	}
+	
+	@PutMapping("/updatetestcases")
+	public TestCases updateTestCases(@RequestBody TestCases testCases){
+		
+		return testCasesServices.updateTestcases(testCases);
+	}
+	
+	@DeleteMapping("/deletetestcases/{id}")
+	public HttpStatus deleteQuestion(@PathVariable Long id) {
+		
+		testCasesServices.deleteTestCasesById(id);
+		return HttpStatus.OK;
 	}
 
 }
--- admin-service/src/main/java/com/feuji/adminservice/service/TestCasesServices.java
+++ admin-service/src/main/java/com/feuji/adminservice/service/TestCasesServices.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.feuji.adminservice.repo.CodingQuestionRepository;
 import com.feuji.adminservice.repo.TestCasesRepository;
 import com.feuji.commonmodel.CodingQuestion;
+import com.feuji.commonmodel.Question;
 import com.feuji.commonmodel.TestCases;
 
 @Service
@@ -21,19 +22,41 @@ public class TestCasesServices {
 	private CodingQuestionRepository codingQuestionRepository;
 	
 
-public void addTestCases(List<TestCases> testCasesList, Long qid) {
+	public void addTestCases(TestCases testCases, Long qid) {
 		
 		CodingQuestion codingQuestion = codingQuestionRepository.findById(qid).get();
-		testCasesList.stream().forEach((testCases) ->{
-										testCases.setCodingQuestion(codingQuestion);
-										testCasesRepository.save(testCases);
-										});
-		
+//		testCasesList.stream().forEach((testCases) ->{
+//										testCases.setCodingQuestion(codingQuestion);
+//										testCasesRepository.save(testCases);
+//										});
+		testCases.setCodingQuestion(codingQuestion);
+		testCases.setStatus("active");
+		testCasesRepository.save(testCases);
 	}
 	
 	public List<TestCases> getAllTestCases(Long id) {
 		List<TestCases> list = testCasesRepository.findByCodingQuestionId(id);
 		return list;
 	}
+
+	public void deleteTestCasesById(Long id) {
+		TestCases testCases = testCasesRepository.findById(id).get();
+		testCases.setStatus("inactive");
+		testCasesRepository.saveAndFlush(testCases);
+		
+	}
+
+	
+	
+	public TestCases updateTestcases(TestCases testCases) {
+		TestCases testCases1 = testCasesRepository.findById(testCases.getId()).get();
+
+		testCases1.setInput(testCases.getInput());
+		testCases1.setExpectedOutput(testCases.getExpectedOutput());
+	
+		return testCasesRepository.saveAndFlush(testCases1);
+	}
+	
+	
 
 }
